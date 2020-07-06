ea0fc4754374bc5b742347da3ea8379a4f5f7743
Harrison Brown <htb25@cam.ac.uk>
2020-07-06 15:48:10 -0500

    grid rainbow 5M

--- grid_rainbow.py
+++ grid_rainbow.py
@@ -77,7 +77,7 @@ def gen_config(update_horizon, min_replay_history, update_period, target_update_
 # evaluation_steps = 1000
 # num_iterations = 2
 
-grid_dir = 'grid_1/'
+grid_dir = 'grid_5M/'
 grid_file = grid_dir + 'results.csv'
 create_folder_if_not_exists(grid_dir)
 
@@ -127,9 +127,9 @@ min_replay_histories = [500, 5000, 20000]
 update_periods = [1, 4]
 target_update_periods = [8000]
 learning_rate = 0.09
-num_training_steps = 500000
+num_training_steps = 5000000
 evaluation_steps = 500000
-num_iterations = 2
+num_iterations = 1
 
 
 
