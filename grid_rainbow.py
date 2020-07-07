b2767d81693a1163e640d7879e3de7c8e4e01f23
Harrison Brown <htb25@cam.ac.uk>
2020-07-07 00:31:26 -0500

    grid rainbow 10m

--- grid_rainbow.py
+++ grid_rainbow.py
@@ -102,10 +102,10 @@ create_folder_if_not_exists(grid_dir)
 # num_iterations = 2
 
 # Run 3
-update_horizons = [1,3]
-min_replay_histories = [500, 5000, 20000]
-update_periods = [1, 4]
-target_update_periods = [1000]
+update_horizons = [3]
+min_replay_histories = [5000]
+update_periods = [4]
+target_update_periods = [4000, 8000]
 learning_rate = 0.09
 num_training_steps = 10000000
 evaluation_steps = 500000
